66a77994c48e28918709b1da19d48d90727dbc09
puniiitchaudhary <141457034+puniiitchaudhary@users.noreply.github.com>
2024-07-25 00:44:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const emailLink = document.querySelector('.contact-info a');
-    emailLink.addEventListener('mouseover', () => {
-        emailLink.style.color = '#ff6347'; // Change color on hover
-    });
-    emailLink.addEventListener('mouseout', () => {
-        emailLink.style.color = ''; // Revert to original color
-    });
-});
+
